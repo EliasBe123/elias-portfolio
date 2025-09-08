778734982bec4c81894f5289258cc3346585fa13
Elias Benjaminsson <eliasv.benjaminsson@gmail.com>
2025-09-08 08:23:16 +0200

    docker_test4

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import path from 'node:path';
 import compression from 'compression';
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const PORT = Number(process.env.PORT ?? 3000);
